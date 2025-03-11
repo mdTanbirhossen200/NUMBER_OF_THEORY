406ff01df02e9309f48999c20af822c72c14f8a8
Tanbir <ta6703921@gmail.com>
2025-03-11 07:53:11 +0600

    almost

--- Gcd Queries.cpp
+++ Gcd Queries.cpp
@@ -1,54 +1,55 @@
-//  NAME OF ALLAH//
+// NAME OF ALLAH//
 
-#include<bits\stdc++.h>
+#include <bits\stdc++.h>
 using namespace std;
-#define faster() ios_base :: sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define faster()                   \
+    ios_base ::sync_with_stdio(0); \
+    cin.tie(0);                    \
+    cout.tie(0);
 #define endl '\n'
 
-int gcd(int a,int b)
+int gcd(int a, int b)
 {
-    if(b == 0)
-    return a;
-    
-        return gcd(b,a % b);
-    
+    if (b == 0)
+        return a;
+
+    return gcd(b, a % b);
 }
 
 int main()
 {
     int t;
-    cin>>t;
+    cin >> t;
 
-    while(t--)
+    while (t--)
     {
-        int n,q;
-        cin>>n>>q;
+        int n, q;
+        cin >> n >> q;
         int arr[n];
 
-        for(int i = 1; i <= n; i++)
-            cin>>arr[i];
-        
+        for (int i = 1; i <= n; i++)
+            cin >> arr[i];
+
         int pre[n + 2];
         int pos[n + 3];
 
         pre[0] = 0;
-        for(int i = 1; i <= n; i++)
-        pre[i] = gcd(pre[i - 1],arr[i]);
+        for (int i = 1; i <= n; i++)
+            pre[i] = gcd(pre[i - 1], arr[i]);
 
         pos[n + 1] = 0;
-        for(int i = n; i >= 1; i--)
+        for (int i = n; i >= 1; i--)
         {
-            pos[i] = gcd(pos[i + 1],arr[i]);
+            pos[i] = gcd(pos[i + 1], arr[i]);
         }
         while (q--)
         {
-           int left,right;
+            int left, right;
 
-           cin>>left>>right;
-           int ans = gcd(pre[left - 1],pos[right + 1]);
-           cout << ans << endl;
+            cin >> left >> right;
+            int ans = gcd(pre[left - 1], pos[right + 1]);
+            cout << ans << endl;
         }
-        
     }
     return 0;
 }
@@ -57,5 +58,5 @@ int main()
 // 3 3
 // 2 6 9
 // 1 1
-//2 2
+// 2 2
 // 3 3
